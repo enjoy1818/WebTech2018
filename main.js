1dc1c718a99a1e240f78c3012bf2ca76ed3f73dc
enjoy1818 <32461841+enjoy1818@users.noreply.github.com>
2018-10-16 19:01:23 +0900

    dropdown

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function showHide(){
+    var state = document.getElementById("dropdown");
+    if(state.style.display == "none"){
+        state.style.display = "flex";
+    }else{
+        state.style.display = "none";
+    }
+};
